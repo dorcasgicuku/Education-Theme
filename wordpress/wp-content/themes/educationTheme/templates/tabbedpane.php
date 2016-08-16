36b0a0385c33e805b1ff53141e6aa92893cffd7d
dorcasgicuku <dorcausgicuku@gmail.com>
2016-08-16 17:02:57 +0300

    some styling added

--- wordpress/wp-content/themes/educationTheme/templates/tabbedpane.php
+++ wordpress/wp-content/themes/educationTheme/templates/tabbedpane.php
@@ -25,7 +25,32 @@
                 </div>
             </td>
             
-            <td id="otherTd" class="other-td-section">Other things</td>
+            <td id="otherTd" class="other-td-section">
+           <table> <tr>
+              <td><h1>Explanation about the university</h1>
+                  <p>
+                    MMUST holds the view that science and technology is an indispensable tool in harnessing resources of nature for sustainable development and creating an environment in which human beings can survive and realize their potential. Thus, the human person has the moral obligation to creatively improve the environment. This can be achieved through a well-conceptualized educational, scientific and technological package through which every human being must rightfully experience and acquire the tools to facilitate this mission.
+                </p>
+                                      <p>
+                        <input type="submit" value="More" name="university-more-button">
+                      </p>
+                </td>
+            </tr>
+              <tr>
+              <td>
+                   <h2>Historical Background</h2>
+                     <p>
+                        The University was founded through Harambee spirit on 5th January, 1972, as Western College of Arts and Applied Sciences (WECO) under the stewardship of .the former fiery MP, Masinde Muliro. As chairman of the project executive committee, Muliro worked with Prof. Reuben J. Olembo (then Head, Department of Botany University of Nairobi) as secretary.
+
+                        The establishment of the College was in response to the needs of the people of Western province to have a College which would provide a training ground for technical manpower requirements for the province and the nation. The College was also expected to inculcate a sense of self-help and self-reliance among its students and facilitate active participation of youth in national development
+                      </p>
+                      <p>
+                        <input type="submit" value="More" name="background-more-button">
+                      </p>
+                      </td>
+                </tr>
+                </table>
+         </td>
        </tr>
     </table>
 
