5ed50a39079aa50e27564c03001c6cfa6ed61632
dorcasgicuku <dorcausgicuku@gmail.com>
2016-08-18 16:15:41 +0300

    styling

--- wordpress/wp-content/themes/educationTheme/templates/tabbedpane.php
+++ wordpress/wp-content/themes/educationTheme/templates/tabbedpane.php
@@ -1,33 +1,33 @@
-<section id="panes" class="pane-section">
-   
-   
-                <div id="mmust-news" class="news">
+<section class="mmust-news-section">
+    <div class="mmust-news-div">
+              <div id="mmust-latest-news" class="news">
                     <h1>Latest News</h1>
                          <div id="recent" class="recent-news">
                              <h2>Heading 1</h2>
                                 <p>This part gives a brief explanation <br>of what the heading  one above is all about.<br>To view more details use the button above</p>
                                    <input type="submit" name="submit" value="more"/>
                          </div>
+                  <hr>
 
                        <div id="recent2" class="other-news">
                           <h2>Heading 2</h2>
                            <p>This part gives a brief explanation <br>of what the heading  two above is all about.<br>To view more details use the button above</p>
                               <input type="submit" name="submit" value="more"/>
                          </div>
-
+                  <hr>
                       <div id="recent3" class="other-news">
                        <h2>Heading 3</h2>
                         <p>This part gives a brief explanation <br>of what the heading  three above is all about.<br>To view more details use the button above</p>
                         <input type="submit" name="submit" value="more"/>
                       </div>
                 </div>
-         
+         <div id="university-explanation" class="mmust explanation">
             <h1>Explanation about the university</h1>
                   <p>
                     MMUST holds the view that science and technology is an indispensable tool in harnessing resources of nature for sustainable development and creating an environment in which human beings can survive and realize their potential. Thus, the human person has the moral obligation to creatively improve the environment. This can be achieved through a well-conceptualized educational, scientific and technological package through which every human being must rightfully experience and acquire the tools to facilitate this mission.
-            
+              </p>
                         <input type="submit" value="More" name="university-more-button">
-                      </p>
+                    
               
                    <h2>Historical Background</h2>
                      <p>
@@ -38,5 +38,6 @@
                       <p>
                         <input type="submit" value="More" name="background-more-button">
                       </p>
-                    
+                   </div>
+           </div>
 </section>
