0cec8e145214ea6bf2d5f9364d2a457ec248d600
dorcasgicuku <dorcausgicuku@gmail.com>
2016-08-31 14:34:09 +0300

    used DRY to minimise the lines of code

--- wordpress/wp-content/themes/educationTheme/templates/tabbedpane.php
+++ wordpress/wp-content/themes/educationTheme/templates/tabbedpane.php
@@ -6,36 +6,37 @@
           <p>This part gives a brief explanation <br>of what the heading  one above is all about.<br>To view more details use the button above</p>
           
           <input type="submit" name="submit" value="more"/>
-          <hr>
+       
 
-         <h2>Heading 4442</h2>
-                  <p>This part gives a brief explanation <br>of what the heading  two above is all about.<br>To view more details use the button above</p>
+         <h3>Heading 4442</h3>
+                  <p>This part gives a brief explanation <br>of what the heading  two above is all about.
+                  <br>To view more details use the button above</p>
                               <input type="submit" name="submit" value="more"/>
-          <hr>
- <h2>Heading 3</h2>
+      
+ <h4>Heading 3</h4>
              <p>This part gives a brief explanation <br>of what the heading  three above is all about.<br>To view more details use the button above</p>
                         <input type="submit" name="submit" value="more"/>
-          <hr>
+         
 </div>
 
 
         <div class="mmust-news-div mmust-explanation">
-            <h1>Explanation about the university</h1>
+            <h5>Explanation about the university</h5>
                   <p>
                     MMUST holds the view that science and technology is an indispensable tool in harnessing resources of nature for sustainable development and creating an environment in which human beings can survive and realize their potential. Thus, the human person has the moral obligation to creatively improve the environment. This can be achieved through a well-conceptualized educational, scientific and technological package through which every human being must rightfully experience and acquire the tools to facilitate this mission.
               </p>
                         <input type="submit" value="More" name="university-more-button">
                     
               
-                   <h2>Historical Background</h2>
+                   <h6>Historical Background</h6>
                      <p>
                         The University was founded through Harambee spirit on 5th January, 1972, as Western College of Arts and Applied Sciences (WECO) under the stewardship of .the former fiery MP, Masinde Muliro. As chairman of the project executive committee, Muliro worked with Prof. Reuben J. Olembo (then Head, Department of Botany University of Nairobi) as secretary.
 
                         The establishment of the College was in response to the needs of the people of Western province to have a College which would provide a training ground for technical manpower requirements for the province and the nation. The College was also expected to inculcate a sense of self-help and self-reliance among its students and facilitate active participation of youth in national development
                       </p>
-                      <p>
+                    
                         <input type="submit" value="More" name="background-more-button">
-                      </p>
+                      
                    </div>
           
 </section>
