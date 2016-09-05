8e5b5664ddc1d46f07b8b2a0d8a85114a7b787e4
Dorcas <dorcasgicuku>
2016-09-05 13:23:32 +0300

    refactored the html code

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'educationTheme');
+define('DB_NAME', 'EducationTheme');
 
 /** MySQL database username */
 define('DB_USER', 'root');
--- wordpress/wp-content/themes/educationTheme/templates/tabbedpane.php
+++ wordpress/wp-content/themes/educationTheme/templates/tabbedpane.php
@@ -1,42 +1,69 @@
 <section class="mmust-news-section">
-
 <div class="mmust-news-div mmust-latest-news">
-   <p id="latest-news"><h1>Latest News</h1></p>
-     <h2>Heading 1</h2>
-          <p>This part gives a brief explanation <br>of what the heading  one above is all about.<br>To view more details use the button above</p>
-          
-          <input type="submit" name="submit" value="more"/>
-       
 
-         <h3>Heading 4442</h3>
-                  <p>This part gives a brief explanation <br>of what the heading  two above is all about.
-                  <br>To view more details use the button above</p>
-                              <input type="submit" name="submit" value="more"/>
-      
- <h4>Heading 3</h4>
-             <p>This part gives a brief explanation <br>of what the heading  three above is all about.<br>To view more details use the button above</p>
-                        <input type="submit" name="submit" value="more"/>
-         
-</div>
+     <h2>Latest news</h2>
+
+        <div class="article">
+
+            <p class="title">Heading 1</p>
+
+            <div class="text">
+
+                <p>This part gives a brief explanation of what the heading one above is all about. To view more details use the button above</p>
+
+            </div>
+
+           <button>More</button>
+
+        </div>
+
+        <div class="article">
+
+            <p class="title">Heading 2</p>
+
+            <div class="text">
+
+                <p>This part gives a brief explanation of what the heading two above is all about. To view more details use the button above</p>
+
+            </div>
+
+           <button>More</button>
+
+        </div>
 
 
-        <div class="mmust-news-div mmust-explanation">
-            <h5>Explanation about the university</h5>
-                  <p>
-                    MMUST holds the view that science and technology is an indispensable tool in harnessing resources of nature for sustainable development and creating an environment in which human beings can survive and realize their potential. Thus, the human person has the moral obligation to creatively improve the environment. This can be achieved through a well-conceptualized educational, scientific and technological package through which every human being must rightfully experience and acquire the tools to facilitate this mission.
-              </p>
-                        <input type="submit" value="More" name="university-more-button">
-                    
-              
-                   <h6>Historical Background</h6>
-                     <p>
-                        The University was founded through Harambee spirit on 5th January, 1972, as Western College of Arts and Applied Sciences (WECO) under the stewardship of .the former fiery MP, Masinde Muliro. As chairman of the project executive committee, Muliro worked with Prof. Reuben J. Olembo (then Head, Department of Botany University of Nairobi) as secretary.
+            <p class="title">Heading 3</p>
 
-                        The establishment of the College was in response to the needs of the people of Western province to have a College which would provide a training ground for technical manpower requirements for the province and the nation. The College was also expected to inculcate a sense of self-help and self-reliance among its students and facilitate active participation of youth in national development
-                      </p>
-                    
-                        <input type="submit" value="More" name="background-more-button">
-                      
-                   </div>
+            <div class="text">
+
+                <p>This part gives a brief explanation of what the heading three above is all about. To view more details use the button above</p>
+
+            </div>
+
+           <button>More</button>
+
+        </div>
+
+    <div class="explain-text">
+
+        <p class="tittle">Explanation about the university</p>
+        <p>
+            MMUST holds the view that science and technology is an indispensable tool in harnessing resources of nature for sustainable development and creating an environment in which human beings can survive and realize their potential. Thus, the human person has the moral obligation to creatively improve the environment. This can be achieved through a well-conceptualized educational, scientific and technological package through which every human being must rightfully experience and acquire the tools to facilitate this mission.-->
+        </p>
+
+    </div>
+
+
+    <div class="explain-text">
+        <p class="tittle">Historical Background</p>
+        <p>
+            The University was founded through Harambee spirit on 5th January, 1972, as Western College of Arts and Applied Sciences (WECO) under the stewardship of .the former fiery MP, Masinde Muliro. As chairman of the project executive committee, Muliro worked with Prof. Reuben J. Olembo (then Head, Department of Botany University of Nairobi) as secretary.-->
+
+                            The establishment of the College was in response to the needs of the people of Western province to have a College  </p>
+
+                </div>
+
+                </div>
+
           
 </section>
